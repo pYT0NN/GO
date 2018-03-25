d3bf53c558729a42f821b723e42fbe90531d29cf
pYT0NN <bustmepure@gmail.com>
2018-03-25 17:46:09 +0200

    safety update

--- Board.java
+++ Board.java
@@ -12,7 +12,7 @@ int pointsB = 0; //Punkte black
 Scanner sc; //Zum Einlesen
 
 
-public Board(Scanner sc, int n) //Übergabe der Spielbrettgröße und erzeugen des Boards
+public Board(Scanner sc, int n) //Übergabe der Spielbrettgröße und Erzeugen des Boards
 {
         this.sc = sc;
         this.n = n;
@@ -23,30 +23,37 @@ public Board(Scanner sc, int n) //Übergabe der Spielbrettgröße und erzeugen d
 public void move()
 {
 
-        if(white) System.out.println("Weiß ist am Zug");
+        if(white) System.out.println("Weiß ist am Zug"); //Zugbeginn
         else System.out.println("Schwarz ist am Zug");
+
         InCheck read = new InCheck(sc, n, brett);
-        int[] zs = read.getZeileUndSpalte();
+        int[] zs = read.getZeileUndSpalte(); //Überprüft die Eingabe
         int zeile = zs[0];
         int spalte = zs[1];
+
         brett[zeile][spalte] = new Stone(white); //Stelle markieren
 }
 
 public void kick(){
         Kick temp = new Kick(n, brett);
-        int[] pointsWB = Kick.andReturnPoints();
+        int[] pointsWB = Kick.andReturnPoints(); //Kickt alle Steine die tot sind
+
+        pointsW += pointsWB[0]; //Gibt Weiß Punkte für geschlagene Steine
+        pointsB += pointsWB[1]; //Gibt Schwarz Punkte für geschlagene Steine
+}
+
+public int[][] count(){ //Zählt Teritorium Punkte
+        Teritory counter = new Teritory(n, brett, used);
+        int[] pointsWB = Teritory.pointsWB();
         pointsW += pointsWB[0];
         pointsB += pointsWB[1];
 }
 
-public int[][] count(){
-        Teritory.
+public void draw(){ //Ausgabe des Spielbretts
+        Out.draw(n, brett);
 }
 
-public void next(){
+public void next(){ //Nächste Spieler
         white = !white;
-}
-public void draw(){
-        Out.draw(n, brett);
 }
 }
--- Teritory.java
+++ Teritory.java
@@ -1,30 +1,72 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Teritory {
-static int n;
-static boolean[][] egroup; //Als Gruppenmarkierung der Teritory Felder
-boolean[][] used; //Bereits gezählte Felder
 
-public Teritory(int n, Stone[][] brett){
+static int n;
+static Stone[][] brett; //Spielbrett
+static boolean[][] egroup; //Als Gruppenmarkierung der Teritory Felder
+static boolean[][] used; //Bereits gezählte Felder
+
+public Teritory(int n, Stone[][] brett, boolean[][] used){
+        this.n = n;
+        this.brett = brett;
+        this.used = used;
         this.egroup = new boolean[n][n];
-        this.used = new boolean[n][n];
 }
 
-public void markZone(int n, int i, int j){   //Gruppe der leeren Steine finden und markieren
+public static int[] pointsWB(){
+        for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                        markZone(i, j);
+                }
+        }
+}
+
+public static void markZone(int i, int j){   //Gruppe der leeren Steine finden und markieren
         if(brett[i][j] == null) {
-                egroup[i][j] = true;   //jedes Teritory Feld markieren was der umschlossenen Gruppe ist
+                egroup[i][j] = true;   //jedes betretene Teritory Feld markieren was umschlossen ist
 
                 //rekursiv anliegende Felder als Teil der Gruppe markieren
                 if(i != 0) {   //nach oben
-                        if(brett[i-1][j] == null && !egroup[i-1][j]) Teritory(i-1, j);
+                        if(brett[i-1][j] == null && !egroup[i-1][j]) markZone(i-1, j);
                 }
                 if(i < n-1) {   //nach unten
-                        if(brett[i+1][j] == null && !egroup[i+1][j]) Teritory(i+1, j);
+                        if(brett[i+1][j] == null && !egroup[i+1][j]) markZone(i+1, j);
                 }
                 if(j != 0) {   //nach links
-                        if(brett[i][j-1] == null && !egroup[i][j-1]) Teritory(i, j-1);
+                        if(brett[i][j-1] == null && !egroup[i][j-1]) markZone(i, j-1);
                 }
                 if(j < n-1) {   //nach rechts
-                        if(brett[i][j+1] != null && !egroup[i][j+1]) Teritory(i, j+1);
+                        if(brett[i][j+1] != null && !egroup[i][j+1]) markZone(i, j+1);
                 }
+        }
+}
+
+public static boolean isZoneWhite(int i, int j){
+        boolean white;
+        while(egroup[i][j] && i < n) {         //Zu einem Feld der markierten Gruppe gehen
+                while(egroup[i][j] && j < n) {
+                        if(!egroup[i][j]) {
+
+                        }
+                        j++;
+                }
+
+                i++;
+        }
+
+        if(i != 0) {                         //nach oben
+                white = brett[i-1][j].isWhite();
+        }
+        else if(i < n-1) {                         //nach unten
+                white = brett[i+1][j].isWhite();
+        }
+        else if(j != 0) {                         //nach links
+                white = brett[i][j-1].isWhite();
+        }
+        else if(j < n-1) {                         //nach rechts
+                white = brett[i][j+1].isWhite();
         }
 }
 }
